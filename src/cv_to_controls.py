3bdd297b7b604b5f062ddddfac808c17b5711952
Grace Wu <grace.b.wu@gmail.com>
2020-03-13 04:46:54 -0400

    structuralizing and ros things

--- src/cv_to_controls.py
+++ src/cv_to_controls.py
@@ -1,27 +1,106 @@
-def controls(circle, axis):
-    speed = None
-    if axis == 'x':
-        if circle[0] > resizedw/3 and circle[0] < 2*resizedw/3:
-            if circle[0] < resizedw/2 + 5 and circle[0] > resizedw/2 - 5:
-                speed = 'stop'
-            else:
-                speed = 'slow'
-        else: 
-            speed = "fast"
-    elif axis == 'z':
-        if circle[1] > resizedh/3 and circle[1] < 2*resizedh/3:
-            if circle[1] < resizedh/2 + 5 and circle[1] > resizedh/2 - 5:
-                speed = 'stop'
-            else:
-                speed = 'slow'
-        else: 
-            speed = "fast"
-    elif axis == 'y':
-        if circle[2] > slowrad:
-            if circle[2] > stoprad:
-                speed = 'stop'
-            else:
-                speed = "slow"
-        else:
-            speed = 'fast'
-    return speed
+#!/usr/bin/env python
+lastx = [0.1,1]
+lasty = [0.1,1]
+lastz = [0.1,1]
+
+def controlx(coordinate):
+    maxspeed = 1000
+    disttoslow = 100
+    resizedw = 500
+    tolerance = 5
+    pub_xctl = rospy.Publisher('xstuff', ******?*****, queue_size=10)
+    rospy.init_node('x_speedanddir', anonymous=True)
+    if coordinate == 0:
+        speed = 0.9 * lastx[0]
+        direction = lastx[1]
+    else if coordinate > resizedw/2 + disttoslow:
+        speed = maxspeed
+        direction = -1
+    else if coordinate < resizedw/2 - disttoslow:
+        speed = maxspeed
+        direction = 1
+    else if coordinate <= resizedw/2 + disttoslow and coordinate >= resizedw/2 + tolerance:
+        speed = abs(coordinate - resizedw/2)/disttoslow * maxspeed
+        direction = -1
+    else if coordinate <= resizedw/2 + disttoslow and coordinate >= resizedw/2 + tolerance:
+        speed = abs(coordinate - resizedw/2)/disttoslow * maxspeed
+        direction = 1
+    else:
+        speed = 0
+        direction = 0
+    lastx[0] = speed
+    lastx[1] = direction
+    pub_xctl.publish([speed,direction])
+
+def listenerx():
+    rospy.init_node('listenerx', anonymous=True)
+    rospy.Subscriber("xstuff", float32, controlx)
+    rospy.spin()
+
+def controlz(coordinate):
+    maxspeed = 1000
+    disttoslow = 100
+    resizedh = 500
+    tolerance = 5
+    pub_zctl = rospy.Publisher('zstuff', ******?*****, queue_size=10)
+    rospy.init_node('z_speedanddir', anonymous=True)
+    if coordinate == 0:
+        speed = 0.9 * lastz[0]
+        direction = lastz[1]
+    else if coordinate > resizedh/2 + disttoslow:
+        speed = maxspeed
+        direction = -1
+    else if coordinate < resizedh/2 - disttoslow:
+        speed = maxspeed
+        direction = 1
+    else if coordinate <= resizedh/2 + disttoslow and coordinate >= resizedh/2 + tolerance:
+        speed = abs(coordinate - resizedh/2)/disttoslow * maxspeed
+        direction = -1
+    else if coordinate <= resizedh/2 + disttoslow and coordinate >= resizedh/2 + tolerance:
+        speed = abs(coordinate - resizedh/2)/disttoslow * maxspeed
+        direction = 1
+    else:
+        speed = 0
+        direction = 0
+    lastz[0] = speed
+    lastz[1] = direction
+    pub_zctl.publish([speed,direction])
+
+def listenerz():
+    rospy.init_node('listenerz', anonymous=True)
+    rospy.Subscriber("zstuff", float32, controlz)
+    rospy.spin()
+
+def controly(radius):
+    maxspeed = 1000
+    rtoslow = 50
+    maxr = 100
+    tolerance = 5
+    pub_yctl = rospy.Publisher('ystuff', ******?*****, queue_size=10)
+    rospy.init_node('y_speedanddir', anonymous=True)
+    if radius = 0:
+        speed = 0.9 * lastx[0]
+        direction = lastx[1]
+    else if radius > maxr - rtoslow:
+        speed = maxspeed
+        direction = -1
+    else if radius < maxr + rtoslow:
+        speed = maxspeed
+        direction = 1
+    else if radius <= maxr + rtoslow and radius >= maxr + tolerance:
+        speed = abs(coordinate - resizedw/2)/disttoslow * maxspeed
+        direction = -1
+    else if radius >= maxr - rtoslow and radius <= maxr - tolerance:
+        speed = abs(coordinate - resizedw/2)/disttoslow * maxspeed
+        direction = 1
+    else:
+        speed = 0
+        direction = 0
+    lasty[0] = speed
+    lasty[1] = direction
+    pub_yctl.publish([speed,direction])
+
+def listenery():
+    rospy.init_node('listenery', anonymous=True)
+    rospy.Subscriber("ystuff", float32, controly)
+    rospy.spin()
--- src/cvstuff.py
+++ src/cvstuff.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+#!/usr/bin/env python
 """
 Created on Fri Feb 14 20:28:19 2020
 
@@ -10,57 +11,87 @@ import imutils
 import glob
 import os
 from pathlib import Path
-from datetime import datetime
+import rospy
+from std_msgs.msg import String
 
-#parameters
-imagepath = 'C:/Users/Grace Wu/m19/src/img/*.JPG'
-resizedw = 500
-resizedh = 375
-
-def findcircle(imagepath):
-    #import image
-    list_of_files = glob.glob(imagepath) # * means all if need specific format then *.csv
-    latest_file = max(list_of_files, key=os.path.getctime)
-    image = cv2.imread(latest_file)
-    #make picture smaller
-    resized = imutils.resize(image, width=resizedw)
-    #make colour filter
-    hsv = cv2.cvtColor(resized, cv2.COLOR_BGR2HSV)
-    lower_range = np.array([60,90,90])
-    upper_range = np.array([120,320,120])
-    mask = cv2.inRange(hsv,lower_range,upper_range)
-    img = cv2.bitwise_and(resized, resized, mask=mask)
-    cv2.imshow('Image1', mask)
-    cv2.waitKey(0)
-    final = resized.copy()
-    #detect circles in the image 
-    circles = cv2.HoughCircles(mask, cv2.HOUGH_GRADIENT, 0.5, 41, param1=70, param2=12 , minRadius=0,maxRadius=175)
-    biggest = [0,0]
-    index = 0
-    #show all circles
-    if circles is not None:
-        for i in circles[0,:]:
-            # draw the outer circle
-            if i[2] > biggest[1]:
-                biggest[1] = i[2]
-                biggest[0] = index
-            cv2.circle(img,(i[0],i[1]),i[2],(0,255,0),2)
-            # draw the center of the circle
-            cv2.circle(img,(i[0],i[1]),2,(0,0,255),3)
-            index += 1
-        cv2.imshow("HoughCircles", img)
-        cv2.waitKey()
-    #show biggest circle
-        cv2.circle(final,(circles[0,biggest[0]][0],circles[0,biggest[0]][1]),circles[0,biggest[0]][2],(0,255,0),2)
-        cv2.imshow("HoughCircles", final)
-        cv2.waitKey()
-        cv2.destroyAllWindows()
-        return circles[0,biggest[0]]
-    else: 
-        print("error!") 
-        cv2.destroyAllWindows()
-
-circle = findcircle(imagepath)
-print(circle)
+def findcircle(imagepath,level):
+    imagepath = 'C:/Users/Grace Wu/m19/src/img/*.JPG'
+    resizedw = 500
+    resizedh = 375
+    maxbluerad = 100
+    maxredrad = 100
+    maxgreenrad = 100
+    tolerance = 5
+    xaligned = False
+    zaligned = False
+    yaligned = False
+    b = [np.array([60,90,90]), np.array([120,320,120]), maxbluerad]
+    r = [np.array([60,90,90]), np.array([120,320,120]), maxredrad]
+    g = [np.array([60,90,90]), np.array([120,320,120]), maxgreenrad]
+    #declare ros node
+    pubx = rospy.Publisher('xstuff', float32, queue_size=10)
+    pubz = rospy.Publisher('zstuff', float32, queue_size=10)
+    puby = rospy.Publisher('ystuff', float32, queue_size=10)
+    rospy.init_node('circlecords', anonymous=True)
+    rate = rospy.Rate(10) # 10hz
+    #loop for each color
+    colours = [b,r,g]
+    for i in colours:
+        #loop until all dimensions aligned
+        while xaligned == False or zaligned == False or yaligned == False:
+            #import image
+            list_of_files = glob.glob(imagepath) # * means all if need specific format then *.csv
+            latest_file = max(list_of_files, key=os.path.getctime)
+            image = cv2.imread(latest_file)
+            #make picture smaller
+            resized = imutils.resize(image, width=resizedw)
+            #make colour filter
+            hsv = cv2.cvtColor(resized, cv2.COLOR_BGR2HSV)
+            mask = cv2.inRange(hsv,i[0],i[1])
+            img = cv2.bitwise_and(resized, resized, mask=mask)
+            cv2.imshow('Image1', mask)
+            cv2.waitKey(0)
+            final = resized.copy()
+            #detect circles in the image 
+            circles = cv2.HoughCircles(mask, cv2.HOUGH_GRADIENT, 0.5, 41, param1=70, param2=12 , minRadius=0,maxRadius=175)
+            biggest = [0,0]
+            index = 0
+            #show all circles
+            if circles is not None:
+                for i in circles[0,:]:
+                    # draw the outer circle
+                    if i[2] > biggest[1]:
+                        biggest[1] = i[2]
+                        biggest[0] = index
+                    cv2.circle(img,(i[0],i[1]),i[2],(0,255,0),2)
+                    # draw the center of the circle
+                    cv2.circle(img,(i[0],i[1]),2,(0,0,255),3)
+                    index += 1
+                cv2.imshow("HoughCircles", img)
+                cv2.waitKey()
+                #show biggest circle
+                cv2.circle(final,(circles[0,biggest[0]][0],circles[0,biggest[0]][1]),circles[0,biggest[0]][2],(0,255,0),2)
+                cv2.imshow("HoughCircles", final)
+                cv2.waitKey()
+                cv2.destroyAllWindows()
+                #evaluate position
+                if biggest[0][0] < resizedw/2 + tolerance and biggest[0][0] > resizedw/2 - tolerance:
+                    xaligned = True
+                if biggest[0][1] < resizedh/2 + tolerance and biggest[0][1] > resizedh/2 - tolerance:
+                    zaligned = True
+                if biggest[0][2] < i[2] + tolerance and biggest[0][2] > i[2] - tolerance:
+                    yaligned = True
+                #publish circle coordinates
+                pubx.publish(circles[0,biggest[0]][0])
+                pubz.publish(circles[0,biggest[0]][1])
+                puby.publish(circles[0,biggest[0]][2])
+                rate.sleep()
+            else: 
+                print("warning! no circles found") 
+                pubx.publish(0)
+                pubz.publish(0)
+                puby.publish(0)
+                rate.sleep()
+                cv2.destroyAllWindows()
 
         
